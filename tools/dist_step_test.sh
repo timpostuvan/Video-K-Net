9ad000aaf4acd0564d561f7a110d1b04990f3458
timpostuvan <tim.postuvan@gmail.com>
2023-05-05 21:55:31 +0100

    Update inference script

--- tools/dist_step_test.sh
+++ tools/dist_step_test.sh
@@ -3,8 +3,9 @@
 CONFIG=$1
 CHECKPOINT=$2
 GPUS=$3
+RESULTS_DIR=$4
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/test_step.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
+    $(dirname "$0")/test_step.py $CONFIG $CHECKPOINT --launcher pytorch --show_dir $RESULTS_DIR ${@:5}
