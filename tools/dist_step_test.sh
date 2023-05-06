d423d359709c16f0a3f20c4320facc583ee98d36
timpostuvan <tim.postuvan@gmail.com>
2023-05-06 21:27:03 +0100

    Minor fix

--- tools/dist_step_test.sh
+++ tools/dist_step_test.sh
@@ -8,4 +8,4 @@ PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/test_step.py $CONFIG $CHECKPOINT --launcher pytorch --show_dir $RESULTS_DIR ${@:5}
+    $(dirname "$0")/test_step.py $CONFIG $CHECKPOINT --launcher pytorch --show-dir $RESULTS_DIR ${@:5}
