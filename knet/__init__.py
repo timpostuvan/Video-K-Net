8bd31cfd666cc6758d1f29e9f4e80df2054cb320
timpostuvan <tim.postuvan@gmail.com>
2023-05-12 20:51:02 +0100

    Add adaptive kernel update with concatenation and skip connections

--- knet/__init__.py
+++ knet/__init__.py
@@ -1,2 +1,3 @@
 from .kernel_updator_concatenate import KernelUpdatorConcatenate
 from .kernel_updator_skip import KernelUpdatorSkip
+from .kernel_updator_concatenate_skip import KernelUpdatorConcatenateSkip
