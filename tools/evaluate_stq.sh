e6c8968513d0cb62103a5546c3401f3c51392e0f
timpostuvan <tim.postuvan@gmail.com>
2023-05-17 13:24:13 +0100

    Rename evaluation scripts

--- tools/evaluate_stq.sh
+++ tools/evaluate_stq.sh
@@ -3,4 +3,4 @@
 RESULTS_DIR=$1
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python ./tools/eval_dstq_step.py $RESULTS_DIR
+python ./tools/evaluate_stq.py $RESULTS_DIR
--- tools/evaluate_vpq.sh
+++ tools/evaluate_vpq.sh
@@ -3,4 +3,4 @@
 RESULTS_DIR=$1
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python ./tools/eval_dvpq_step.py $RESULTS_DIR
+python ./tools/evaluate_vpq.py $RESULTS_DIR
